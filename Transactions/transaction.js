f326c08a3480f2353edb26090c1dee79d90dff50
Amay Kataria <amkatari@microsoft.com>
2018-07-29 19:08:41 +0900

    Add the timeout feature to transaction.

--- Transactions/transaction.js
+++ Transactions/transaction.js
@@ -10,71 +10,25 @@ const web3 = new Web3('https://ropsten.infura.io/v3/24b152bca7704bf39741eed18597
 const account1 = '0x50f8be0A651a5F930c460BF7675fa0493F087967';
 const account2 = '0x4Ae52740cB65c23e2634B3aFfBeb9f090f852Ebe';
 
+// Reset these keys on another machine. 
 const privateKey1 = Buffer.from(process.env.PRIVATE_KEY_1, 'hex'); 
 const privateKey2 = Buffer.from(process.env.PRIVATE_KEY_2, 'hex'); 
+
+// Initial transactions.
+let numTransactions = 0;
 
 var udpPort = new osc.UDPPort({
     localAddress: "127.0.0.1",
     localPort: 8080,
     remoteAddress: "127.0.0.1",
-    remotePort: 8081
+    remotePort: 8081 // Port to send data on.
 });
 
-// Open UDP port for sending data. 
-udpPort.open();
-
-const inputData = web3.utils.toHex("I Am Alive. July 26, 2018. 11:06pm. Amay Kataria."); 
-// Price I'm willing to pay per transaction.
-const gasPriceInWei = web3.utils.toWei('10', 'gwei');
-let maxGasUnits;
-web3.eth.getTransactionCount(account1, (err, txCount) => {
-    web3.eth.estimateGas({to: account2, data: inputData}, (err, result)=> {
-        // Estimated gas result.
-        maxGasUnits = result;
-
-        // Build the transaction. 
-        const txObject = {
-            nonce: web3.utils.toHex(txCount),
-            to: account2,
-            value: web3.utils.toHex(web3.utils.toWei('0', 'ether')),
-            gasLimit: web3.utils.toHex(maxGasUnits),
-            gasPrice: web3.utils.toHex(gasPriceInWei),
-            data: inputData
-        };
-
-        // // Sign the transaction. 
-        const tx = new Tx(txObject);
-        tx.sign(privateKey1);
-
-        // Serialize the transaction. Create raw transaction. 
-        const serializedTransaction = tx.serialize(); 
-        const raw = '0x' + serializedTransaction.toString('hex');
-
-        // Broadcast the transaction
-        web3.eth.sendSignedTransaction(raw)
-        .on('transactionHash', hash => {
-            console.log("Transaction broadcasted: " + hash);
-        })
-        .then(receipt => {
-            // Get the actual block
-            const block = web3.eth.getBlock(receipt.blockHash, (err, block) => {
-                let txData = {
-                    blockNum: receipt.blockNumber,
-                    blockHash: receipt.blockHash,
-                    timestamp: block.timestamp,
-                    txHash: receipt.transactionHash,
-                    txCost: receipt.gasUsed * web3.utils.fromWei(gasPriceInWei, 'ether'),
-                    input: web3.utils.hexToString(inputData)
-                };
-                console.log(txData);
-                sendOscMessage(txData);
-            });
-        })
-        .catch(err => {
-            console.log("Unexpected error: " + err);
-        });
-    });
+// Events
+udpPort.on("ready", function () {
+    console.log("UDP port ready.");
 });
+
 
 function sendOscMessage(txData) {
     const dataToSend = txData.blockNum + "," + txData.blockHash + "," + txData.timestamp + "," + txData.txHash + "," + txData.txCost + "," + txData.input;
@@ -90,9 +44,85 @@ function sendOscMessage(txData) {
         ]
     });
 
-    console.log("Transaction sent successfully to OF");
+    console.log("Transaction sent successfully to OF.");
 }
 
-udpPort.on("ready", function () {
-    console.log("UDP port ready.");
-});
+function broadcastTransaction() {
+    // Don't broadcast the transaction. 
+    if (numTransactions == maxTransactionsPerDay) {
+        console.log("That's it for today. More to come tomorrow.");
+
+        // Reset transactions and create a timeout for 12 hours. 
+        numTransactions = 0; 
+        setTimeout(broadcastTransaction, subsequentDelay);
+        return;
+    }
+
+    const inputData = web3.utils.toHex("I Am Alive. July 26, 2018. 11:06pm. Amay Kataria."); 
+    // Price I'm willing to pay per transaction.
+    const gasPriceInWei = web3.utils.toWei('10', 'gwei');
+
+    let maxGasUnits;
+    web3.eth.getTransactionCount(account1, (err, txCount) => {
+        web3.eth.estimateGas({to: account2, data: inputData}, (err, result)=> {
+            // Estimated gas result.
+            maxGasUnits = result;
+
+            // Build the transaction. 
+            const txObject = {
+                nonce: web3.utils.toHex(txCount),
+                to: account2,
+                value: web3.utils.toHex(web3.utils.toWei('0', 'ether')),
+                gasLimit: web3.utils.toHex(maxGasUnits),
+                gasPrice: web3.utils.toHex(gasPriceInWei),
+                data: inputData
+            };
+
+            // // Sign the transaction. 
+            const tx = new Tx(txObject);
+            tx.sign(privateKey1);
+
+            // Serialize the transaction. Create raw transaction. 
+            const serializedTransaction = tx.serialize(); 
+            const raw = '0x' + serializedTransaction.toString('hex');
+
+            // Broadcast the transaction
+            web3.eth.sendSignedTransaction(raw)
+            .on('transactionHash', hash => {
+                console.log("Transaction broadcasted: " + hash);
+            })
+            .then(receipt => {
+                numTransactions++; 
+
+                // Get the actual block
+                const block = web3.eth.getBlock(receipt.blockHash, (err, block) => {
+                    let txData = {
+                        blockNum: receipt.blockNumber,
+                        blockHash: receipt.blockHash,
+                        timestamp: block.timestamp,
+                        txHash: receipt.transactionHash,
+                        txCost: receipt.gasUsed * web3.utils.fromWei(gasPriceInWei, 'ether'),
+                        input: web3.utils.hexToString(inputData)
+                    };
+                    console.log(txData);
+                    sendOscMessage(txData);
+
+                    // Wait for transactionDelay before next transaction. 
+                    setTimeout(broadcastTransaction, transactionDelay);
+                });
+            })
+            .catch(err => {
+                console.log("Unexpected error: " + err);
+            });
+        });
+    });
+}
+
+const maxTransactionsPerDay = 3; 
+const transactionDelay = 5000; // 10 seconds for now. Ideally 25 minutes.
+const subsequentDelay = 10000; // Should be 12 hours.  
+
+
+// Open UDP port for sending data. 
+udpPort.open();
+broadcastTransaction();
